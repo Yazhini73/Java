3d5da89118b74ed13e5817a483cc9449731d95b6
Yazhini73 <yazhinik73@gmail.com>
2025-10-09 00:44:25 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+public static void main(String[] args)
+{
+ArrayList <String> con= new ArrayList<String>();
+con.add("zhini");
+con.add("shafi");
+con.add("bava");
+con.add("deepika");
+con.add("jj");
+for(String con1:con)
+{
+System.out.println("contact list"+con1);
+}
+System.out.println("Retrieve;"+con.get(3))
+System.out.println("Update:"+set(4,"Oops"));
+System.out.println(Remove:"+con.remove(1)):
+}}
